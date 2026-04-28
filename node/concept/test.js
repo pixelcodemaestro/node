6a20666a53147c62cc7151de28ded850577d577f
pixelcodemaestro <pixelcodemaestro@gmail.com>
2026-04-28 12:23:03 +0530

    test

---
+++ node/concept/test.js
@@ -0,0 +1,30 @@
+const name = "prasanth";
+const age = 24;
+
+console.log(`my name is ${name} and i am ${age} years old`);
+
+
+
+
+function add(a, b) {
+    return a+b;
+}
+
+const result = add(5, 3);
+console.log(result);
+
+
+
+
+const fruits=["apple", "banana", "orange"];
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]); 
+
+
+
+const fruits1 = ["apple", "banana", "orange"];
+fruits1[0] = "red";
+console.log(fruits1);
+
+
